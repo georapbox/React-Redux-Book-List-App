f216db86579389185de1910a4a773bb7f57af41b
George Raptis <georapbox@gmail.com>
2017-04-20 17:59:14 +0300

    create books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+function BooksReducer() {
+  return [
+    {title: 'Javascript: The Good Parts'},
+    {title: 'Harry Potter'},
+    {title: 'The Dark Tower'},
+    {title: 'Eloquent Ruby'}
+  ];
+}
+
+export default BooksReducer;

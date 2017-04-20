06cd66eaadfcbfc64a1e107eb7db298c0633ea56
George Raptis <georapbox@gmail.com>
2017-04-20 18:00:07 +0300

    BookList container

---
+++ src/containers/book_list.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map(book => {
+      return (
+        <li className="list-group-item" key={book.title}>
+          {book.title}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    );
+  }
+}
+
+BookList.propTypes = {
+  books: PropTypes.array
+};
+
+function mapStateToProps(state) {
+  // Whatever is returned will show up
+  // as props inside of BookList
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
